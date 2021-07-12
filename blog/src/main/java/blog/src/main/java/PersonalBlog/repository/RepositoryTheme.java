443f6ee4ce30e58ad4013b1342595f3a4e9bfdd8
gilsonmirandaj <gilsonmirandajunior2@gmail.com>
2021-07-12 09:08:38 -0300

    add

--- blog/src/main/java/blog/src/main/java/PersonalBlog/repository/RepositoryTheme.java
+++
@@ -1,9 +0,0 @@
-package blog.src.main.java.PersonalBlog.repository;
-
-import blog.src.main.java.PersonalBlog.model.Theme;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RepositoryTheme extends JpaRepository<Theme, Long> {
-    public List<Theme> findAllByDescriptionContainingIgnoreCase(String description);
-}
